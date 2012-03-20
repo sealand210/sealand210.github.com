53b3dc5363dea0b444841c1b265bbc144d93a03c
Evan Hung <mail2evanhung@gmail.com>
2012-03-20 20:23:56 +0800

    update

--- source/downloads/code/test.js
+++ source/downloads/code/test.js
@@ -10,9 +10,6 @@ var undefined,
     simpleExpr = /^#?([\w-]+)$/,
     idExpr     = /^#/,
     tagExpr    = /<([\w:]+)/,
-    slice      = function (e) { return [].slice.call(e, 0); };
-    try { var a = slice(document.documentElement.childNodes)[0].nodeType; }
-    catch(e){ slice = function (e) { var ret=[]; for (var i=0; e[i]; i++)
         ret.push(e[i]); return ret; }; }
 
 window.x$ = window.xui = xui = function(q, context) {
