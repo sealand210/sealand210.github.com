bbc7d5b8170801e0666185dc4419e34b8df81433
Evan Hung <mail2evanhung@gmail.com>
2012-03-22 20:41:42 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 rake generate
+rake deploy
+git add .
+git commit -m 'update'
+git push origin bak
+git add .
+git commit -am 'update'
+git push origin source
+git config branch.master.remote heroku
 git add .
 git commit -m 'update'
 git push heroku master
 git config branch.master.remote origin
-git add .
-git commit -m 'update'
-git push origin master
-git config branch.master.remote heroku
